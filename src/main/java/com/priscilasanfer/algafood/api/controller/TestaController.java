80714baef156284e7b7d44d540df729c9279ea32
Priscila Ferreira <priscilasanfer@gmail.com>
2021-02-23 14:07:10 -0300

    5.1. Implementando consultas JPQL em repositórios

---
+++ src/main/java/com/priscilasanfer/algafood/api/controller/TestaController.java
@@ -0,0 +1,25 @@
+package com.priscilasanfer.algafood.api.controller;
+
+import com.priscilasanfer.algafood.domain.model.Cozinha;
+import com.priscilasanfer.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teste")
+public class TestaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping("cozinha/por-nome")
+    public List<Cozinha> cozinhasPorNome(@RequestParam String nome){
+        return cozinhaRepository.consultarPorNome(nome);
+
+    }
+}
--- src/main/java/com/priscilasanfer/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/priscilasanfer/algafood/domain/repository/CozinhaRepository.java
@@ -1,12 +1,15 @@
 package com.priscilasanfer.algafood.domain.repository;
 
 import com.priscilasanfer.algafood.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface CozinhaRepository {
 
 	List<Cozinha> listar();
+	List<Cozinha> consultarPorNome(String nome);
 	Cozinha buscar(Long id);
 	Cozinha salvar(Cozinha cozinha);
 	void remover(Long id);
