1636cc7743430bd03ee176311b8b8bfe0a0bdb21
Priscila Ferreira <priscilasanfer@gmail.com>
2021-03-08 14:27:11 -0300

    6.3. Analisando o impacto do relacionamento muitos-para-muitos na REST API

--- src/main/java/com/priscilasanfer/algafood/domain/model/Restaurante.java
+++ src/main/java/com/priscilasanfer/algafood/domain/model/Restaurante.java
@@ -1,5 +1,6 @@
 package com.priscilasanfer.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -36,6 +37,7 @@ public class Restaurante {
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
             joinColumns = @JoinColumn(name = "restaurante_id"),
