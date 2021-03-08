d37fc4df9c519d5c9426155cec4b5cec8954e2ee
Priscila Ferreira <priscilasanfer@gmail.com>
2021-03-08 13:39:28 -0300

    6.1. Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/priscilasanfer/algafood/domain/model/Cozinha.java
+++ src/main/java/com/priscilasanfer/algafood/domain/model/Cozinha.java
@@ -1,5 +1,6 @@
 package com.priscilasanfer.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,6 +10,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @JsonRootName("cozinha")
 @Data
@@ -24,4 +28,7 @@ public class Cozinha {
 	@Column(nullable = false)
 	private String nome;
 
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 }
