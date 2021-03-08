3a998cb445e4ddb90ffbbd75ea400cb9b40694d5
Priscila Ferreira <priscilasanfer@gmail.com>
2021-03-08 12:22:54 -0300

    5.19. Injetando o próprio repositório na implementação customizada e a anotação @Lazy

--- src/main/java/com/priscilasanfer/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/priscilasanfer/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,7 +1,11 @@
 package com.priscilasanfer.algafood.infrastructure.repository;
 
 import com.priscilasanfer.algafood.domain.model.Restaurante;
+import com.priscilasanfer.algafood.domain.repository.RestauranteRepository;
 import com.priscilasanfer.algafood.domain.repository.RestauranteRepositoryQueries;
+import com.priscilasanfer.algafood.infrastructure.repository.spec.RestauranteSpecs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -16,11 +20,18 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.priscilasanfer.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.priscilasanfer.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
     @PersistenceContext
     private EntityManager manager;
+
+    @Autowired
+    @Lazy
+    private RestauranteRepository restauranteRepository;
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
@@ -49,5 +60,10 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
         return query.getResultList();
 
+    }
+
+    @Override
+    public List<Restaurante> findComFreteGratis(String nome) {
+        return restauranteRepository.findComFreteGratis(nome);
     }
 }
