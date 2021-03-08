c01e6c3b1aa653a9145aeb7c6f410ffb674ac340
Priscila Ferreira <priscilasanfer@gmail.com>
2021-03-08 13:59:10 -0300

    6.2. Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/java/com/priscilasanfer/algafood/domain/model/Restaurante.java
+++ src/main/java/com/priscilasanfer/algafood/domain/model/Restaurante.java
@@ -9,27 +9,37 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
 
-	@EqualsAndHashCode.Include
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(nullable = false)
-	private String nome;
+    @Column(nullable = false)
+    private String nome;
 
-	@Column(name = "taxa_frete", nullable = false)
-	private BigDecimal taxaFrete;
+    @Column(name = "taxa_frete", nullable = false)
+    private BigDecimal taxaFrete;
 
-	@ManyToOne
-	@JoinColumn(name = "cozinha_id", nullable = false)
-	private Cozinha cozinha;
+    @ManyToOne
+    @JoinColumn(name = "cozinha_id", nullable = false)
+    private Cozinha cozinha;
+
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento",
+            joinColumns = @JoinColumn(name = "restaurante_id"),
+            inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formasDePagamentos = new ArrayList<>();
 
 }
