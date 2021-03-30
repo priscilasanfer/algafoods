56270039b56115552993885f3a42a2490afa69f8
Priscila Ferreira <priscilasanfer@gmail.com>
2021-03-30 09:55:30 -0300

    8.19. Desafio: usando o formato de problemas no corpo de respostas

--- src/main/java/com/priscilasanfer/algafood/api/exceptionHandler/ProblemType.java
+++ src/main/java/com/priscilasanfer/algafood/api/exceptionHandler/ProblemType.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
-    ENTIDADE_NAO_ENCONTRADA ("/entidade-nao-encontrada", "Entidade não encontrada");
+    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+    ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+    ENTIDADE_USO("/entidade-em-uso", "Entidade em uso");
 
     private String title;
     private String uri;
-
 
     ProblemType(String path, String title) {
         this.uri = "https://www.algafood.com.br" + path;
