524afe0203186fd609b4bfe0ce5eeae98d89f478
Priscila Ferreira <priscilasanfer@gmail.com>
2021-03-31 08:58:24 -0300

    9.3. Desafio: tratando exception de violação de constraints de validação

--- src/main/java/com/priscilasanfer/algafood/api/exceptionHandler/ProblemType.java
+++ src/main/java/com/priscilasanfer/algafood/api/exceptionHandler/ProblemType.java
@@ -9,7 +9,8 @@ public enum ProblemType {
     ENTIDADE_USO("/entidade-em-uso", "Entidade em uso"),
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
     PARAMETRO_INVALIDO("/parametro-invalido", "Parametro inválido"),
-    ERRO_DE_SISTEMA("/erro-de-sistema", "Houve um erro no sistema");
+    ERRO_DE_SISTEMA("/erro-de-sistema", "Houve um erro no sistema"),
+    DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos");
 
     private String title;
     private String uri;
