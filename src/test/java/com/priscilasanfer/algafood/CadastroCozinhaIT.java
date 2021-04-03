f88bc042bab1154643c677f3b1f7ad073ef2a9c0
Priscila Ferreira <priscilasanfer@gmail.com>
2021-04-03 12:45:51 -0300

    10.9. Validando o corpo da resposta HTTP

--- src/test/java/com/priscilasanfer/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/priscilasanfer/algafood/CadastroCozinhaIT.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
@@ -29,6 +31,20 @@ public class CadastroCozinhaIT {
                 .get()
             .then()
                 .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveConter4CozinhasQuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+            .when()
+                .get()
+            .then()
+                .body("", hasSize(4))
+                .body("nome", hasItems("Indiana", "Brasileira"));
     }
 
 }
