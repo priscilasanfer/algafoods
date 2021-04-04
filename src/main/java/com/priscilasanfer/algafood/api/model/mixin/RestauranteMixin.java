67c8054a0fbed00ec09fc2535a6038633ccfa679
Priscila Ferreira <priscilasanfer@gmail.com>
2021-04-04 08:50:59 -0300

    11.7. Configurando e refatorando o projeto para usar UTC

--- src/main/java/com/priscilasanfer/algafood/api/model/mixin/RestauranteMixin.java
+++ src/main/java/com/priscilasanfer/algafood/api/model/mixin/RestauranteMixin.java
@@ -7,7 +7,7 @@ import com.priscilasanfer.algafood.domain.model.Endereco;
 import com.priscilasanfer.algafood.domain.model.FormaPagamento;
 import com.priscilasanfer.algafood.domain.model.Produto;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 public abstract class RestauranteMixin {
@@ -18,11 +18,11 @@ public abstract class RestauranteMixin {
     @JsonIgnore
     private Endereco endereco;
 
-    @JsonIgnore
-    private LocalDateTime dataCadastro;
+    //@JsonIgnore
+    private OffsetDateTime dataCadastro;
 
-    @JsonIgnore
-    private LocalDateTime dataAtualizacao;
+   // @JsonIgnore
+    private OffsetDateTime dataAtualizacao;
 
     @JsonIgnore
     private List<FormaPagamento> formasPagamentos;
