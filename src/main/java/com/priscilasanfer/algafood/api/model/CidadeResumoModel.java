f81ec8fa13d93b8b1215c517562bdabcde8df9bc
Priscila Ferreira <priscilasanfer@gmail.com>
2021-10-05 00:52:36 -0300

    12.6. Adicionando endereço no modelo da representação do recurso de restaurante

---
+++ src/main/java/com/priscilasanfer/algafood/api/model/CidadeResumoModel.java
@@ -0,0 +1,12 @@
+package com.priscilasanfer.algafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CidadeResumoModel {
+    private Long id;
+    private String nome;
+    private String estado;
+}
--- src/main/java/com/priscilasanfer/algafood/core/ModelMapperConfig.java
+++ src/main/java/com/priscilasanfer/algafood/core/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.priscilasanfer.algafood.core;
 
+import com.priscilasanfer.algafood.api.model.EnderecoModel;
+import com.priscilasanfer.algafood.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,15 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        var modelMapper = new ModelMapper();
+
+        // modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class).addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+
+        var enderecoToEnderecoModelTypedMap = modelMapper
+                .createTypeMap(Endereco.class, EnderecoModel.class)
+                .addMapping(enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+                        (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(String.valueOf(value)));
+
+        return modelMapper;
     }
 }
