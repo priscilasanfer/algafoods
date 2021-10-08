1222fb1dadd8ef0ad6d9edf1f89b68433bc90a48
Priscila Ferreira <priscilasanfer@gmail.com>
2021-10-08 11:27:11 -0300

    12.11. Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/priscilasanfer/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/priscilasanfer/algafood/domain/service/CadastroUsuarioService.java
@@ -11,6 +11,8 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 public class CadastroUsuarioService {
 
@@ -21,6 +23,13 @@ public class CadastroUsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
+        usuarioRepository.detach(usuario);
+        Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
+            throw new NegocioException(
+                    String.format("Já existe um usuário cadastrado com o e-mail informado %s", usuario.getEmail()));
+        }
         return usuarioRepository.save(usuario);
     }
 
--- src/main/java/com/priscilasanfer/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
+++ src/main/java/com/priscilasanfer/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -24,4 +24,9 @@ public class CustomJpaRepositoryImpl<T, ID>
         T entity = manager.createQuery(jpql, getDomainClass()).setMaxResults(1).getSingleResult();
         return Optional.ofNullable(entity);
     }
+
+    @Override
+    public void detach(T entity) {
+        manager.detach(entity);
+    }
 }
