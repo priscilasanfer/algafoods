2ac6fa27eb7993114351fcf0eef3a45ec55eb53d
Priscila Ferreira <priscilasanfer@gmail.com>
2021-10-17 12:11:11 -0300

    corrige nome de classe de testes do JPA

--- src/main/java/com/priscilasanfer/algafood/jpa/InclusaoCozinhaMain.java
+++ src/main/java/com/priscilasanfer/algafood/jpa/InclusaoCozinhaMain.java
@@ -7,7 +7,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class InclusãoCozinhaMain {
+public class InclusaoCozinhaMain {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
                 .web(WebApplicationType.NONE)
