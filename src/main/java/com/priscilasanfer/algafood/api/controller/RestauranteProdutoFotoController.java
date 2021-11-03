230a57e6d4ed1f9c276fe025b81761bef69c927e
Priscila Ferreira <priscilasanfer@gmail.com>
2021-11-02 21:33:18 -0300

    14.3. Validando o tamanho máximo do arquivo

--- src/main/java/com/priscilasanfer/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/priscilasanfer/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -2,10 +2,12 @@ package com.priscilasanfer.algafood.api.controller;
 
 import com.priscilasanfer.algafood.api.model.input.FotoProdutoInput;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import javax.validation.Valid;
 import java.nio.file.Path;
 import java.util.UUID;
 
@@ -15,7 +17,7 @@ public class RestauranteProdutoFotoController {
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
-                              FotoProdutoInput fotoProdutoInput) {
+                              @Valid FotoProdutoInput fotoProdutoInput) {
 
         var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
         var arquivoFoto = Path.of("/Users/prissantos/Desktop/catalogo", nomeArquivo);
--- src/main/java/com/priscilasanfer/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/priscilasanfer/algafood/api/model/input/FotoProdutoInput.java
@@ -1,13 +1,20 @@
 package com.priscilasanfer.algafood.api.model.input;
 
+import com.priscilasanfer.algafood.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class FotoProdutoInput {
 
+    @NotNull
+    @FileSize(max = "30KB")
     private MultipartFile arquivo;
+    @NotBlank
     private String descricao;
 }
