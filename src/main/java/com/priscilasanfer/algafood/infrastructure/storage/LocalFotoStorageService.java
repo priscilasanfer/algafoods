6b59b27e6010989584fe171c429f2af6fb7209ee
Priscila Ferreira <priscilasanfer@gmail.com>
2021-11-12 09:26:22 -0300

    14.26. Selecionando a implementação do serviço de storage de fotos

--- src/main/java/com/priscilasanfer/algafood/infrastructure/storage/LocalFotoStorageService.java
+++ src/main/java/com/priscilasanfer/algafood/infrastructure/storage/LocalFotoStorageService.java
@@ -3,13 +3,11 @@ package com.priscilasanfer.algafood.infrastructure.storage;
 import com.priscilasanfer.algafood.core.storage.StorageProperties;
 import com.priscilasanfer.algafood.domain.service.FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-@Service
 public class LocalFotoStorageService implements FotoStorageService {
 
     @Autowired
